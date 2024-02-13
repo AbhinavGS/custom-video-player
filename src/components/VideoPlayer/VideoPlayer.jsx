5e6251c18d9040455d059177d922874fc48a864f
Abhinav Sorate <abhinavsorate01@gmail.com>
2024-02-13 23:59:37 +0530

    implement play pause functionality

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,28 @@
 import "./VideoPlayer.scss";
 
+import PlayArrowRoundedIcon from "@mui/icons-material/PlayArrowRounded";
+import PauseRoundedIcon from "@mui/icons-material/PauseRounded";
+import SkipNextRoundedIcon from "@mui/icons-material/SkipNextRounded";
+import VolumeUpRoundedIcon from "@mui/icons-material/VolumeUpRounded";
+import { useState, useRef } from "react";
+
 const VideoPlayer = () => {
+  const [isPlaying, setIsPlaying] = useState("false");
+  const videoRef = useRef(null);
+
+  const togglePlayPause = () => {
+    if (videoRef.current.paused) {
+      videoRef.current.play();
+    } else {
+      videoRef.current.pause();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
   return (
     <div className="video-player">
       <video
-        controls
+        ref={videoRef}
         poster="https://storage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg"
       >
         <source
@@ -13,6 +31,21 @@ const VideoPlayer = () => {
         />
         Your browser does not support HTML video.
       </video>
+      <div className="control-panel">
+        <button className="btn-play-pause" onClick={togglePlayPause}>
+          {isPlaying ? (
+            <PlayArrowRoundedIcon sx={{ fontSize: 30 }} />
+          ) : (
+            <PauseRoundedIcon sx={{ fontSize: 30 }} />
+          )}
+        </button>
+        <button>
+          <SkipNextRoundedIcon sx={{ fontSize: 30 }} />
+        </button>
+        <button>
+          <VolumeUpRoundedIcon sx={{ fontSize: 30 }} />
+        </button>
+      </div>
     </div>
   );
 };
