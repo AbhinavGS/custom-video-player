ed6b16d112603d55ab78e69f952b245134e0486a
Abhinav Sorate <abhinavsorate01@gmail.com>
2024-02-13 22:35:16 +0530

    add dummy video

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,7 +1,20 @@
 import "./VideoPlayer.scss";
 
 const VideoPlayer = () => {
-  return <div className="video-player">VideoPlayer</div>;
+  return (
+    <div className="video-player">
+      <video
+        controls
+        poster="https://storage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg"
+      >
+        <source
+          src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+          type="video/mp4"
+        />
+        Your browser does not support HTML video.
+      </video>
+    </div>
+  );
 };
 
 export default VideoPlayer;
